bbe6e695fdc620b0950a66c2bfbdf4beaff84f91
unknown <kmcasto@OU140-22.ad.ilstu.edu>
2014-11-13 19:56:35 -0600

    Added ray and hit boilerplate

---
+++ ScenegraphWithLightsTextures/ScenegraphWithLightsTextures/Hit.h
@@ -0,0 +1,44 @@
+#ifndef HIT_H
+#define HIT_H
+
+#include <GL/gl.h>
+#include <vector>
+#include <stack>
+#include "material.h"
+#include "texture.h"
+using namespace std;
+#include <glm/gtc/matrix_transform.hpp>
+
+// there is a glm::normalize you fucking retard, have a nice day :)
+
+class Hit
+{
+public:
+
+	float time;
+	//pt
+	glm::vec4 intersect;
+	//n_pt
+	glm::vec4 normal;
+	Material mat;
+	Texture tex;
+	// add a texturemap at some point?
+
+	float s;
+	float t;
+
+    Hit() {
+
+    }
+
+    ~Hit()
+    {
+
+    }
+
+protected:
+	// woop
+	
+};
+
+#endif // HIT_H
---
+++ ScenegraphWithLightsTextures/ScenegraphWithLightsTextures/Ray.h
@@ -0,0 +1,33 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include <GL/gl.h>
+#include <vector>
+#include <stack>
+#include "material.h"
+#include "texture.h"
+using namespace std;
+#include <glm/gtc/matrix_transform.hpp>
+
+class Ray
+{
+public:
+
+
+    Ray() {
+
+    }
+
+    ~Ray()
+    {
+
+    }
+
+
+
+protected:
+	// woop
+	
+};
+
+#endif // RAY_H
